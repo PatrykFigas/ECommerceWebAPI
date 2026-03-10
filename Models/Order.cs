ce1873023f5450bbe5891513cca6d1a0289054c8
Patryk Figas <patrykf09@wp.pl>
2026-03-10 10:10:19 +0100

    Add EFC and two models - Porduct and Order

---
+++ Models/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAPI.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+
+        public ICollection<Product> Products { get; set; } = new List<Product>();
+    }
+}
