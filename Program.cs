9197d8613b80b5bc75f2e83050b2a1f23aa2fca9
Patryk Figas <patrykf09@wp.pl>
2026-03-10 15:01:36 +0100

    Switch database from LocalDB to SQLite for Azure

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ namespace WebAPI
             builder.Services.AddSwaggerGen();
 
             builder.Services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
